3047d579902c3d8b37afceb142804f238c2421cc
Shadab Khan <shadabkhan7253@gmail.com>
2024-10-27 15:12:05 +0530

    Renderer2 class added to avoid directly manipulating the DOM element

--- src/app/CustomDirective/SetBackground.directive.ts
+++ src/app/CustomDirective/SetBackground.directive.ts
@@ -1,18 +1,25 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[setBackground]',
 })
 export class SetBackground implements OnInit {
   // private element: ElementRef;
+  // private renderer : Renderer2;
 
   // It will automatically create the private filled element and assign the value too.
-  constructor(private element: ElementRef) {
+  constructor(private element: ElementRef, private renderer: Renderer2) {
     // this.element = element;
+    // this.renderer = renderer;
   }
 
   ngOnInit() {
-    this.element.nativeElement.style.backgroundColor = '#36454f';
-    this.element.nativeElement.style.color = 'white';
+    // It is not prefer to diretly manipulate the DOM element in order to prevent this
+    // angular provide Renderer2 class to do this
+    // this.element.nativeElement.style.backgroundColor = '#36454f';
+    // this.element.nativeElement.style.color = 'white';
+
+    this.renderer.setStyle(this.element.nativeElement, 'background', '#36454f');
+    this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
   }
 }
